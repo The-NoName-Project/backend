5378943b9d7187200604886753eb5abb0d2734a3
du4rt3 <al222010329@gmail.com>
2022-08-18 12:51:47 -0500

    Configuracion ruta home

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   //  return $request->user();
-//});
-
+Route::get('/', function (){
+  return response()->json('No Name Project');
+});
 Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
   
